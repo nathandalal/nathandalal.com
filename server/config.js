7af283b587d527ab4bdda73f40d7492a6f71c30c
Nathan Dalal <nathanhdalal@gmail.com>
2016-08-17 23:53:46 -0700

    revised mongo

--- server/config.js
+++ server/config.js
@@ -4,6 +4,6 @@ module.exports = {
     PORT: process.env.PORT || 7777,
     DEVMODE: (process.env.NODE_ENV != 'production'),
     MONGO: {
-        URL: process.env.MONGO_URL
+        URL: process.env.MONGODB_URI || "mongodb://localhost/nathandalal-com-dev"
     }
 }
