5fcb668eb102602b731efe9d0795c0020bccd72f
Nathan Dalal <nathanhdalal@gmail.com>
2016-09-08 10:31:36 -0700

    testing newrelic and new node version

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Nathan Dalal - Personal Website'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '32edbe2a05bcb933b3b3c9ad3ec03e1da80460e5',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
