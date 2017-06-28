78c3627615f585a62722741646c16b2fc4e2752a
Nathan Dalal <nathandalal@172-18-26-233.DYNAPOOL.NYU.EDU>
2017-06-27 22:10:50 -0400

    switched to simple lightweight nunjucks from react

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var express = require('express'),
+    engines = require('consolidate'),
+    app = express(),
+    config = require('./config'),
+    AirtableHandler = require('./server/airtable');
+
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+	Promise.all([
+		AirtableHandler.getExperiences("Education"),
+		AirtableHandler.getExperiences("Code"),
+		AirtableHandler.getExperiences("Fun"),
+		AirtableHandler.getMiscellaneous()
+	])
+	.then(function (data) {
+		console.log(data[0]);
+		res.render('index', {
+			educitems: data[0],
+			codeitems: data[1],
+			funitems: data[2],
+			tagline: data[3][0]["Information"]
+		});
+	})
+});
+
+app.get('*', function(req, res) {
+	res.redirect('/');
+})
+
+app.listen(config.PORT, function () {
+	console.log('> nathandalal.com: app listening on port %s', config.PORT);
+});
--- config.js
+++ config.js
@@ -3,11 +3,8 @@ if (process.env.NODE_ENV != 'production') require('dotenv').load()
 module.exports = {
     PORT: process.env.PORT || 7777,
     DEVMODE: (process.env.NODE_ENV != 'production'),
-    MONGO: {
-        URL: process.env.MONGODB_URI || "mongodb://localhost/nathandalal-com-dev"
-    },
     AIRTABLE: {
     	API_KEY: process.env.AIRTABLE_API_KEY,
-    	BASE: "appOYMihRk3B4e0MV"
+    	BASE: process.env.AIRTABLE_BASE || "appOYMihRk3B4e0MV"
     }
 }
